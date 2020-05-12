ca851ad889d9702f7e12831f3825319167ba2234
Saulo Reis <sauloarth@gmail.com>
2020-05-12 20:36:54 -0300

    Iniciando API.

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const apiRouter = require('./routes/api')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    authSource: 'admin',
+    user: process.env.MONGODB_USER,
+    pass: process.env.MONGODB_PASS
+}
+
+
+mongoose.connect(MONGODB_URL, mongoOptions)
+    .then(() => {
+        if (process.env.NODE_ENV !== 'test') {
+            console.log('Connected to %s - test', MONGODB_URL); // change to test db string
+        }
+    })
+    .catch(err => {
+        console.error('Error on connecting to mongodb:', err)
+    })
+
+const db = mongoose.connection;
+
+app.use('/api/', apiRouter);
+
+
+
+const port = process.env.PORT || '3000';
+app.listen(port, () => {
+    console.log('Server running on port ', port)
+})
