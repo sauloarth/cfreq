a45dcc411875df9a679dcc2836919368a9fad92c
Saulo Reis <sauloarth@gmail.com>
2020-06-08 10:46:00 -0300

    Corrigindo método de alteração de departamentos.

--- api/routes/departamento.js
+++ api/routes/departamento.js
@@ -3,7 +3,7 @@ const DeptoController = require('../controllers/DeptoController');
 
 router.get('/', DeptoController.deptoList);
 router.post('/', DeptoController.deptoCreate);
-router.post('/:id', DeptoController.deptoUpdate);
+router.put('/:id', DeptoController.deptoUpdate);
 router.delete('/:id', DeptoController.deptoDisable);
 
 module.exports = router;
