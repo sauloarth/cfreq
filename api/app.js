58eac94b0750b38b836c3604bd7e79306920323a
Saulo Reis <sauloarth@gmail.com>
2020-06-10 17:05:49 -0300

    Ajustando porta de escuta do servidor.

--- api/app.js
+++ api/app.js
@@ -39,7 +39,7 @@ app.use('/api/', apiRouter);
 
 
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3001';
 app.listen(port, () => {
     console.log('Server running on port ', port)
 })
